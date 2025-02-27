44ed99bf1cb4376e0e7d606ffc2f06557c15024a
Vihar Contractor <171323596+xlostincode@users.noreply.github.com>
2025-02-28 03:07:25 +0530

    Update for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "~", replacement: path.resolve(__dirname, "src") }],
-  },
+    base: "./",
+    plugins: [react()],
+    resolve: {
+        alias: [{ find: "~", replacement: path.resolve(__dirname, "src") }],
+    },
 })
